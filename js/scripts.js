b8cb47e26217cbc79724c73de894ca181fd885ac
Svea Wade <svealinneawade@gmail.com>
2020-11-02 12:17:08 -0800

    initial commit for address book lesson

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+//Business Logic for Address Book
+function AddressBook () {
+this.contacts = [];
+this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId
+}
+
+//Business Logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName
+}
