854eb4280f94f67c1575eccaaabb56d49e745448
Svea Wade <svealinneawade@gmail.com>
2020-11-03 09:00:45 -0800

    added all interface logic to make the code work

--- js/scripts.js
+++ js/scripts.js
@@ -40,8 +40,10 @@ AddressBook.prototype.deleteContact = function (id) {
 function Contact(firstName, lastName, nickName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
-  this.nickName = nickName;
+  //this.nickName = nickName;
   this.phoneNumber = phoneNumber;
+  //this.emailAddress = emailAddress;
+  //this.mailingAddress = mailingAddress;
 }
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName
@@ -49,3 +51,50 @@ Contact.prototype.fullName = function() {
 Contact.prototype.nickNamePhone = function () {
   return this.nickName + " " +this.phoneNumber;
 }
+
+//User Interface Logic
+
+let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay){
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact){
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + "" + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
+function showContact(contactId) {
+  const contact = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button>");
+}
+
+function attachContactListeners() {
+  $("ul#contacts").on("click","li",function(){
+    showContact(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function() {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
+  });
+};
+$(document).ready(function() {
+  attachContactListeners();
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    //const inputtedNickName = $("input#new-nick-name").val();
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  });
+});
